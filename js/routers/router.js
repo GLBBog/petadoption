dbbda1a917116f6aad7e305e8da788efe09e0a6f
andresftenjo <useda.theli@gmail.com>
2014-08-13 15:25:39 -0500

    filter by id

--- js/routers/router.js
+++ js/routers/router.js
@@ -11,23 +11,40 @@ app.AppRouter = Backbone.Router.extend({
     },
 
     list: function (page) {
+
         var p = page ? parseInt(page, 10) : 1;
         var typesList = new TypeCollection();
-        typesList.add([{name: 'Dog'}, {name: 'Cat'}, {name: 'Other'}]);
+        console.log('typesList ' + typesList);
+        typesList.add([{ name: 'Dog' }, { name: 'Cat' }, { name: 'Other'}]);
         var l = new TypesListView({ model: typesList, page: p })
 
         var petList = new PetCollection();
         petList.fetch({ success: function () {
             /*petList = petList.byType("dog");*/
+
+            console.log(utils.asString(petList));
+
             $("#content").html(new PetListView({ model: petList, page: p }).el);
         }
         });
     },
 
     petDetails: function (id) {
-        var pet = new Pet({ id: id });
-        pet.fetch({ success: function () {
-            $("#content").html(new PetView({ model: pet }).el);
+        //var pet = new Pet({ id: id });
+
+        var petList = new PetCollection();
+
+        console.log('pet id ' + id);
+
+        petList.fetch({ success: function () {
+
+            petList = petList.byId(id);
+            console.log(utils.asString(petList));
+
+            $("#content").html(new PetView({ model: petList }).el);
+
+            //console.log('pet ' + utils.asString(petList));
+            //$("#content").html(new PetView({ model: petList }).el);
         }
         });
     },
