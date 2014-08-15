150de8daa1335366a90b75efe9f1d777065262d0
lucarbeta <lucarbeta@hotmail.com>
2014-08-15 14:58:11 -0500

    Valores de formulario

--- js/views/petpublish.js
+++ js/views/petpublish.js
@@ -1,4 +1,7 @@
 window.PetPublishView = Backbone.View.extend({
+    events: {
+        'click #btnSave': 'SavePet'
+    },
     initialize: function () {
         $('#HomePlaceHolder').hide();
         this.render();
@@ -7,5 +10,35 @@ window.PetPublishView = Backbone.View.extend({
         var pets = this.model.models;
         $(this.el).html(this.template(pets[0].toJSON()));
         return this;
+    },
+
+    SavePet: function (e) {
+        // GET INPUTS VALUES
+        var pet = {
+            id: null,
+            name: $('#InputNamePet').val(),
+            gender: $('#SelectPetGender').val(),
+            age: $('#InputAgePet').val(),
+            size: $('#SelectPetSize').val(),
+            activity: $('#SelectPetActivity').val(),
+            type: $('#SelectPetType').val(),
+            country: $('#InputCountryPet').val(),
+            breed: $('#InputBreedPet').val(),
+            region: $('#InputCityPet').val(),
+            vaccinated: $('#CheckVaccPet').val(),
+            details: $('#InputDetailsPet').val(),
+            DefaultPicture: $('#InputPrimaryImage').val(),
+            Gallery: [$('#InputGalleryImage1').val(), $('#InputGalleryImage2').val(), $('#InputGalleryImage3').val()],
+            ContactName: $('#InputUserName').val(),
+            ContactEmail: $('#InputEmail').val(),
+            ContactPhone: $('#InputPhone').val(),
+            ContactPhone2: $('#InputPhone2').val(),
+            ContactAddress: $('#InputAddress').val(),
+            ContactZip: $('#InputZipCode').val()
+        };
+
+        // CREATE THE PRODUCT OBJECT
+        pet = new window.Pet(pet);
+        pet.save();
     }
 });
